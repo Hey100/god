e9c3761938122989deb46fff8527047649f35c90
Michael <michael@topmiller@gmail.com>
2018-05-06 20:47:38 -0700

    latest

--- client/src/components/PoolDetail.js
+++ client/src/components/PoolDetail.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 import Chart from '../components/Chart';
+import moment from 'moment';
 
 class PoolDetail extends Component {
   componentDidMount() {
     this.props.fetchPool(this.props.match.params.id);
-	}
-	componentWillUnmount() {
-		this.props.reset()
-	}
-	
+  }
+  componentWillUnmount() {
+    this.props.reset();
+  }
 
   render() {
     const { pools } = this.props;
     if (!pools.pool || !pools.chart) {
-      return <p>Loading...</p>;
+			return <p>Loading...</p>;
     }
+		const date = moment(pools.pool.date).calendar();
     return (
       <div>
-        <h1 className="text-1">{pools.pool.title}</h1>
-        <Chart chart={pools.chart} user={this.props.auth.user} params={this.props.match.params.id}/>
+        <h1 className="text-1">Title: {pools.pool.title}</h1>
+        <h5>Start Date: {date}</h5>
+        <Chart
+          chart={pools.chart}
+          user={this.props.auth.user}
+          params={this.props.match.params.id}
+        />
+				<div>
+				<p>Leave a comment</p>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+				</div>
       </div>
     );
   }
