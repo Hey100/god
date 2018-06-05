e0b84d9d26f1675b428a3a8a0889b68506a80f1c
Michael <michael@topmiller@gmail.com>
2018-06-04 22:24:27 -0400

    deleted redux-logger

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(thunk, logger));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
