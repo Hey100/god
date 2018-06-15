7d089df886eb0da76488e4794121506e1a73ebd6
Michael <michaeltopmiller@gmail.com>
2018-06-15 16:41:44 -0700

    added dashReducer.test.js

---
+++ client/src/reducers/__tests__/dashReducer.test.js
@@ -0,0 +1,25 @@
+import { FETCHED_PAYMENTS } from '../../actions/types';
+import dashReducer from '../dashReducer';
+
+let INITIAL_STATE;
+beforeEach(() => {
+  INITIAL_STATE = {
+    payments: []
+  };
+});
+
+it('handles actions with type FETCHED_PAYMENTS', () => {
+  const action = {
+    type: FETCHED_PAYMENTS,
+    payload: { 1: 'payment1', 2: 'payment2' }
+  };
+  const newState = dashReducer(INITIAL_STATE, action);
+  expect(newState).toEqual({
+    payments: action.payload
+  });
+});
+
+it('handles actions with an unknown type', () => {
+  const newState = dashReducer([], {});
+  expect(newState).toEqual([]);
+});
